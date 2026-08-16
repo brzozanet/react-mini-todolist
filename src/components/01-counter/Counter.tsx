dfc7399acd2abb0e4f380ff52d730e74cffbf84c
Paweł Brzozowski <pawel@brzoza.net>
2026-08-16 10:50:39 +0200

    Update Counter.tsx

--- src/components/01-counter/Counter.tsx
+++ src/components/01-counter/Counter.tsx
@@ -3,27 +3,15 @@ import { useState } from "react";
 export default function Counter() {
   const [counter, setCounter] = useState(0);
 
-  // const handleButtonUp = () => {
-  //   setCounter((prevState) => prevState + 1);
-  // };
-
-  // const handleButtonDown = () => {
-  //   setCounter((prevState) => prevState - 1);
-  // };
-
-  // const handleButtonReset = () => {
-  //   setCounter(0);
-  // };
-
-  const handleConunterButton = (value: number) => {
+  const updateCounter = (value: number) => {
     setCounter((prevState) => prevState + value);
   };
 
   return (
     <>
       <h3>Projekt 1: Licznik ze stanem (React + TypeScript)</h3>
-      <button onClick={() => handleConunterButton(-1)}>-</button> {counter}{" "}
-      <button onClick={() => handleConunterButton(1)}>+</button>{" "}
+      <button onClick={() => updateCounter(-1)}>-</button> {counter}{" "}
+      <button onClick={() => updateCounter(1)}>+</button>{" "}
       <button onClick={() => setCounter(0)}>wyzeruj</button>
     </>
   );
