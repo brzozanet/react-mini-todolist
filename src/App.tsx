d50b723372917ebe620abcff2a711395adf71660
Paweł Brzozowski <pawel@brzoza.net>
2026-08-16 09:56:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+      </section>
+
+      <section id="next-steps"></section>
+
+      <section id="spacer"></section>
+    </>
+  );
+}
+
+export default App;
