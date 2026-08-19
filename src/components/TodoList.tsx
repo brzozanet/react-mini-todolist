720ad9f112842ea3c8b85dd7df898f8f486fcde7
Paweł Brzozowski <pawel@brzoza.net>
2026-08-19 08:16:54 +0200

    Add TodoList component with state management and Todo type definition

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import type { Todo } from "../types/types";
+
 export default function TodoList() {
-  return <p>Todo lista, która sprawi mi mnóstwo radości 😀</p>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <>
+      <p>Todo lista, która sprawi mi mnóstwo radości 😀</p>
+      <h3>Dodaj zadanie</h3>
+      <h3>Lista zadań (łącznie: {todos.length}, do zrobienia:)</h3>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.content}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
